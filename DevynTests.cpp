c2e33caaa9af8c1337fe94547b08231c1b6cb11f
DC <DCprivate@github.com>
2024-09-19 02:23:36 -0700

    added jmp and lda tests, fixed movi test

--- DevynTests.cpp
+++ DevynTests.cpp
@@ -85,17 +85,15 @@ TEST(EMU4380_UNITTESTS, ADD_TEST_VALID) {
 
 TEST(EMU4380_UNITTESTS, FETCH_ADD_TEST_VALID) {
 
-    // setup memory
-    reg_file[R3] = 13;
-    reg_file[R2] = 12;
-
     // test instruction
     unsigned char add_instr[8] = {
-        ADD, R0, R3, R2, 0x00, 0x00, 0x00, 0x00
+        ADD, 5, 6, 12, 0x00, 0x00, 0x00, 0x00
     };
 
-    reg_file[PC] = 0;  // Set PC to point to the start of memory (address 0)
-    prog_mem = new unsigned char[1024];
+    // setup memory
+    reg_file[PC] = 0;
+    mem_size = 1024;
+    prog_mem = new unsigned char[mem_size];
     std::memcpy(prog_mem, add_instr, sizeof(add_instr));
 
     // fetch
@@ -103,42 +101,36 @@ TEST(EMU4380_UNITTESTS, FETCH_ADD_TEST_VALID) {
     delete[] prog_mem;
 
     // decode
+    // execute
 
     // assert
     EXPECT_EQ(cntrl_regs[OPERATION], ADD);
-    EXPECT_EQ(cntrl_regs[OPERAND_1], R0);
-    EXPECT_EQ(cntrl_regs[OPERAND_2], R3);
-    EXPECT_EQ(cntrl_regs[OPERAND_3], R2);
+    EXPECT_EQ(cntrl_regs[OPERAND_1], 5);
+    EXPECT_EQ(cntrl_regs[OPERAND_2], 6);
+    EXPECT_EQ(cntrl_regs[OPERAND_3], 12);
     EXPECT_EQ(cntrl_regs[IMMEDIATE], 0x00000000);
     EXPECT_EQ(reg_file[PC], 8);
 }
 
 TEST(EMU4380_UNITTESTS, FETCH_ADD_TEST_INVALID) {
 
-    // setup memory
-    reg_file[R3] = 26;
-    reg_file[R2] = 25;
-
     // test instruction
     unsigned char add_instr[8] = {
-        ADD, R0, R3, R2, 0x00, 0x00, 0x00, 0x00
+        ADD, 2, 12, 12, 0x00, 0x00, 0x00, 0x00
     };
 
-    reg_file[PC] = 0;  // Set PC to point to the start of memory (address 0)
-    prog_mem = new unsigned char[1024];
+    // setup memory
+    reg_file[PC] = 6;
+    mem_size = 8;
+    prog_mem = new unsigned char[mem_size];
     std::memcpy(prog_mem, add_instr, sizeof(add_instr));
 
-    // execute
-    ASSERT_FALSE(fetch());
+    // fetch
+    EXPECT_FALSE(fetch());
     delete[] prog_mem;
 
     // assert
-    EXPECT_EQ(cntrl_regs[OPERATION], ADD);
-    EXPECT_EQ(cntrl_regs[OPERAND_1], R0);
-    EXPECT_EQ(cntrl_regs[OPERAND_2], R3);
-    EXPECT_EQ(cntrl_regs[OPERAND_3], R2);
-    EXPECT_EQ(cntrl_regs[IMMEDIATE], 0x00000000);
-    EXPECT_EQ(reg_file[PC], 8);
+    EXPECT_EQ(reg_file[PC], 6);
 }
 
 TEST(EMU4380_UNITTESTS, DECODE_MOV_VALID) {
@@ -165,6 +157,60 @@ TEST(EMU4380_UNITTESTS, DECODE_MOV_VALID) {
     EXPECT_TRUE(decode());
 }
 
+TEST(EMU4380_UNITTESTS, DECODE_JMP_VALID) {
+
+    // test instruction
+    unsigned char jmp_instr[8] = {
+        JMP, 0, 0, 0, 0xFF, 0x00, 0x00, 0x00
+    };
+
+    // setup memory
+    reg_file[PC] = 0;
+    mem_size = 1024;
+    prog_mem = new unsigned char[mem_size];
+    std::memcpy(prog_mem, jmp_instr, sizeof(jmp_instr));
+
+    // fetch
+    cntrl_regs[OPERATION] = prog_mem[reg_file[PC]];
+    cntrl_regs[OPERAND_1] = prog_mem[reg_file[PC] + 1];
+    cntrl_regs[OPERAND_2] = prog_mem[reg_file[PC] + 2];
+    cntrl_regs[OPERAND_3] = prog_mem[reg_file[PC] + 3];
+    cntrl_regs[IMMEDIATE] = static_cast<unsigned int>(prog_mem[4]) |
+                    (static_cast<unsigned int>(prog_mem[5]) << 8) |
+                    (static_cast<unsigned int>(prog_mem[6]) << 16) |
+                    (static_cast<unsigned int>(prog_mem[7]) << 24);
+
+    // decode
+    EXPECT_TRUE(decode());       
+}
+
+TEST(EMU4380_UNITTESTS, DECODE_JMP_INVALID) {
+
+    // test instruction
+    unsigned char jmp_instr[8] = {
+        JMP, 0, 0, 0, 0x00, 0xFF, 0x00, 0xFF
+    };
+
+    // setup memory
+    reg_file[PC] = 0;
+    mem_size = 1024;
+    prog_mem = new unsigned char[mem_size];
+    std::memcpy(prog_mem, jmp_instr, sizeof(jmp_instr));
+
+    // fetch
+    cntrl_regs[OPERATION] = prog_mem[reg_file[PC]];
+    cntrl_regs[OPERAND_1] = prog_mem[reg_file[PC] + 1];
+    cntrl_regs[OPERAND_2] = prog_mem[reg_file[PC] + 2];
+    cntrl_regs[OPERAND_3] = prog_mem[reg_file[PC] + 3];
+    cntrl_regs[IMMEDIATE] = static_cast<unsigned int>(prog_mem[4]) |
+                    (static_cast<unsigned int>(prog_mem[5]) << 8) |
+                    (static_cast<unsigned int>(prog_mem[6]) << 16) |
+                    (static_cast<unsigned int>(prog_mem[7]) << 24);
+
+    // decode
+    EXPECT_FALSE(decode());  
+}
+
 TEST(EMU4380_UNITTESTS, DECODE_MOV_INVALID) {
 
     // test instruction
@@ -187,6 +233,66 @@ TEST(EMU4380_UNITTESTS, DECODE_MOV_INVALID) {
 
     // decode
     EXPECT_FALSE(decode());
+}
+
+TEST(EMU4380_UNITTESTS, DECODE_LDA_VALID) {
+    // test instruction
+    unsigned char lda_instr[8] = {
+        LDA, 1, 0, 0, 0x04, 0x00, 0x00, 0x00
+    };
+
+    // setup memory
+    reg_file[PC] = 0;
+    mem_size = 1024;
+    prog_mem = new unsigned char[mem_size];
+    std::memcpy(prog_mem, lda_instr, sizeof(lda_instr));
+
+    // fetch
+    cntrl_regs[OPERATION] = prog_mem[reg_file[PC]];
+    cntrl_regs[OPERAND_1] = prog_mem[reg_file[PC] + 1];
+    cntrl_regs[OPERAND_2] = prog_mem[reg_file[PC] + 2];
+    cntrl_regs[OPERAND_3] = prog_mem[reg_file[PC] + 3];
+    cntrl_regs[IMMEDIATE] = static_cast<unsigned int>(prog_mem[4]) |
+                    (static_cast<unsigned int>(prog_mem[5]) << 8) |
+                    (static_cast<unsigned int>(prog_mem[6]) << 16) |
+                    (static_cast<unsigned int>(prog_mem[7]) << 24);
+
+    // decode
+    EXPECT_TRUE(decode());
+}
+
+TEST(EMU4380_UNITTESTS, DECODE_LDA_INVALID) {
+    // test instruction
+    unsigned char lda_instr[8] = {
+        LDA, 1, 0, 0, 0x00, 0xFF, 0xFF, 0xFF
+    };
+
+    // setup memory
+    reg_file[PC] = 0;
+    mem_size = 12;
+    prog_mem = new unsigned char[mem_size];
+    std::memcpy(prog_mem, lda_instr, sizeof(lda_instr));
+
+    // fetch
+    cntrl_regs[OPERATION] = prog_mem[reg_file[PC]];
+    cntrl_regs[OPERAND_1] = prog_mem[reg_file[PC] + 1];
+    cntrl_regs[OPERAND_2] = prog_mem[reg_file[PC] + 2];
+    cntrl_regs[OPERAND_3] = prog_mem[reg_file[PC] + 3];
+    cntrl_regs[IMMEDIATE] = static_cast<unsigned int>(prog_mem[4]) |
+                    (static_cast<unsigned int>(prog_mem[5]) << 8) |
+                    (static_cast<unsigned int>(prog_mem[6]) << 16) |
+                    (static_cast<unsigned int>(prog_mem[7]) << 24);
+
+    // decode
+    EXPECT_FALSE(decode());
+}
+
+TEST(EMU4380_UNITTESTS, DECODE_STR_VALID) {
+
+}
+
+TEST(EMU4380_UNITTESTS, DECODE_STR_INVALID) {
+
 }
 
 TEST(EMU4380_INTEGRATEDTEST, ADD_TEST) { // not done
